e4e3bdf7887ea17e5a9f5af7775d948cc1152c31
ruben <rubensousa.mieti@gmail.com>
2017-01-22 22:15:21 +0000

    Start moving preview

--- app/src/main/java/com/github/rubensousa/previewseekbar/sample/MainActivity.java
+++ app/src/main/java/com/github/rubensousa/previewseekbar/sample/MainActivity.java
@@ -2,14 +2,22 @@ package com.github.rubensousa.previewseekbar.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.github.rubensousa.previewseekbar.PreviewSeekBar;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
+
+    private PreviewSeekBar previewSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        previewSeekBar = (PreviewSeekBar) findViewById(R.id.previewSeekBar);
+        View view = findViewById(R.id.previewView);
+        previewSeekBar.setPreviewView(view);
     }
 
 }
