18b056fcbb908dced574437caa8763d2fc18fef4
ruben <rubensousa.mieti@gmail.com>
2017-01-23 02:26:12 +0000

    Create layouts due to needing another framelayout

--- app/src/main/java/com/github/rubensousa/previewseekbar/sample/MainActivity.java
+++ app/src/main/java/com/github/rubensousa/previewseekbar/sample/MainActivity.java
@@ -2,22 +2,19 @@ package com.github.rubensousa.previewseekbar.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
-import com.github.rubensousa.previewseekbar.PreviewSeekBar;
+import com.github.rubensousa.previewseekbar.PreviewSeekBarLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private PreviewSeekBar previewSeekBar;
+    private PreviewSeekBarLayout previewSeekBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        previewSeekBar = (PreviewSeekBar) findViewById(R.id.previewSeekBar);
-        View view = findViewById(R.id.previewView);
-        previewSeekBar.setPreviewView(view);
+        previewSeekBarLayout = (PreviewSeekBarLayout) findViewById(R.id.previewSeekBarLayout);
     }
 
 }
