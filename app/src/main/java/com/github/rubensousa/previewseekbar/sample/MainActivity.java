b4583018c7c989959042910364b06eac0fd5ef1d
ruben <rubensousa.mieti@gmail.com>
2017-01-23 05:45:00 +0000

    Use TextureView for preview

--- app/src/main/java/com/github/rubensousa/previewseekbar/sample/MainActivity.java
+++ app/src/main/java/com/github/rubensousa/previewseekbar/sample/MainActivity.java
@@ -4,27 +4,25 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.View;
 import android.widget.SeekBar;
 
 import com.github.rubensousa.previewseekbar.PreviewSeekBar;
 import com.github.rubensousa.previewseekbar.PreviewSeekBarLayout;
 import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
@@ -36,8 +34,10 @@ import com.google.android.exoplayer2.upstream.HttpDataSource;
 import com.google.android.exoplayer2.util.Util;
 
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, ExoPlayer.EventListener {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
+    private View defaultSurfaceView;
+    private View seekSurfaceView;
     private PreviewSeekBarLayout previewSeekBarLayout;
     private SimpleExoPlayerView playerView;
     private SimpleExoPlayer player;
@@ -48,24 +48,44 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         setContentView(R.layout.activity_main);
         previewSeekBarLayout = (PreviewSeekBarLayout) findViewById(R.id.previewSeekBarLayout);
         playerView = (SimpleExoPlayerView) findViewById(R.id.player_view);
+        seekSurfaceView = findViewById(R.id.surfaceView);
+        PreviewSeekBar seekBar = (PreviewSeekBar) playerView.findViewById(R.id.exo_progress);
+
+       /* ((SurfaceView) seekSurfaceView).setZOrderMediaOverlay(true);
+        ((SurfaceView) seekSurfaceView).getHolder().setFormat(PixelFormat.TRANSPARENT);*/
+        seekBar.addOnSeekBarChangeListener(this);
         player = setupPlayer();
         playerView.setPlayer(player);
+        defaultSurfaceView = playerView.getVideoSurfaceView();
         prepareMedia();
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         // load video progress
+        player.seekTo(player.getDuration() * progress / seekBar.getMax());
     }
 
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
-
+        if (seekSurfaceView instanceof SurfaceView) {
+            ((SurfaceView) seekSurfaceView).setZOrderMediaOverlay(true);
+            player.setVideoSurfaceView((SurfaceView) seekSurfaceView);
+        } else if (seekSurfaceView instanceof TextureView) {
+            player.setVideoTextureView((TextureView) seekSurfaceView);
+        }
     }
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-
+        if (seekSurfaceView instanceof SurfaceView) {
+            ((SurfaceView) seekSurfaceView).setZOrderMediaOverlay(false);
+        }
+        if (defaultSurfaceView instanceof TextureView) {
+            player.setVideoTextureView((TextureView) defaultSurfaceView);
+        } else if (defaultSurfaceView instanceof SurfaceView) {
+            player.setVideoSurfaceView((SurfaceView) defaultSurfaceView);
+        }
     }
 
     private SimpleExoPlayer setupPlayer() {
@@ -94,15 +114,20 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         // Produces Extractor instances for parsing the media data.
         ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
 
+        Uri uri = Uri.parse(getString(R.string.url_dash));
+
         // This is the MediaSource representing the media to be played.
-        MediaSource videoSource = new DashMediaSource(Uri.parse(getString(R.string.url)),
+        /*MediaSource videoSource = new SsMediaSource(uri, buildDataSourceFactory(bandwidthMeter),
+                new DefaultSsChunkSource.Factory(buildDataSourceFactory(bandwidthMeter)),
+                new Handler(), null);*/
+
+        MediaSource videoSource = new DashMediaSource(uri,
                 dataSourceFactory,
                 new DefaultDashChunkSource.Factory(buildDataSourceFactory(bandwidthMeter)),
                 new Handler(), null);
 
         // Prepare the player with the source.
         player.prepare(videoSource);
-        player.addListener(this);
     }
 
     private DataSource.Factory buildDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
@@ -113,35 +138,5 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private HttpDataSource.Factory buildHttpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
         return new DefaultHttpDataSourceFactory(Util.getUserAgent(this, "ExoPlayerDemo"),
                 bandwidthMeter);
-    }
-
-    @Override
-    public void onTimelineChanged(Timeline timeline, Object manifest) {
-
-    }
-
-    @Override
-    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-
-    }
-
-    @Override
-    public void onLoadingChanged(boolean isLoading) {
-
-    }
-
-    @Override
-    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-
-    }
-
-    @Override
-    public void onPlayerError(ExoPlaybackException error) {
-
-    }
-
-    @Override
-    public void onPositionDiscontinuity() {
-
     }
 }
