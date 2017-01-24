ec7737178b5d4d6533b0e37ecce603d4747c1bf7
ruben <rubensousa.mieti@gmail.com>
2017-01-24 23:28:51 +0000

    Add copyright notice

--- app/src/main/java/com/github/rubensousa/previewseekbar/sample/exoplayer/ExoPlayerManager.java
+++ app/src/main/java/com/github/rubensousa/previewseekbar/sample/exoplayer/ExoPlayerManager.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2016 The Android Open Source Project
+ * Copyright 2017 Rúben Sousa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.rubensousa.previewseekbar.sample.exoplayer;
 
 import com.github.rubensousa.previewseekbar.PreviewSeekBarLayout;
@@ -107,13 +124,8 @@ public class ExoPlayerManager implements ExoPlayer.EventListener {
     private SimpleExoPlayer createFullPlayer() {
         TrackSelection.Factory videoTrackSelectionFactory
                 = new AdaptiveVideoTrackSelection.Factory(new DefaultBandwidthMeter());
-
         TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
-
-        // 2. Create a default LoadControl
         LoadControl loadControl = new DefaultLoadControl();
-
-        // 3. Create the player
         SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(playerView.getContext(),
                 trackSelector, loadControl);
         player.setPlayWhenReady(true);
@@ -124,12 +136,8 @@ public class ExoPlayerManager implements ExoPlayer.EventListener {
 
     private SimpleExoPlayer createPreviewPlayer() {
         TrackSelection.Factory videoTrackSelectionFactory = new SingleVideoTrackSelection.Factory();
-
         TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
-
         LoadControl loadControl = new PreviewLoadControl();
-
-        // 3. Create the player
         SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(previewPlayerView.getContext(),
                 trackSelector, loadControl);
         player.setPlayWhenReady(false);
